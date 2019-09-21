50a1c94964039455e0db5b892566e3f061aa49fd
Adam Zawierucha <zawiedev@gmail.com>
2019-09-20 23:21:06 -0500

    making parser handle document internally

--- TenTabsExtension/src/parser.js
+++ TenTabsExtension/src/parser.js
@@ -1,6 +1,6 @@
 //Takes html from a google search and parses it into a table of strings (links)
-export function parseText(doc) {
-    divs = doc.getElementsByClassName("r") //array of divs from webpages
+export function parseText() {
+    divs = docoument.getElementsByClassName("r") //array of divs from webpages
     URLs = Array()
     for (div in divs) {
         URL = div.basURI
