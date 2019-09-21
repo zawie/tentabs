f1c8797b1261cd97a64361d425b43f7075517ed4
Adam Zawierucha <zawiedev@gmail.com>
2019-09-20 23:08:13 -0500

    first iteration of parser

--- TenTabsExtension/src/parser.js
+++ TenTabsExtension/src/parser.js
@@ -1,4 +1,10 @@
 //Takes html from a google search and parses it into a table of strings (links)
-export function pase_text(html) {
-
+export function parse_text(doc) {
+    divs = doc.getElementsByClassName("r") //array of divs from webpages
+    URLs = Array()
+    for (div in divs) {
+        URL = div.basURI
+        URLs.push(URL)
+    }
+    return URLs
 }
