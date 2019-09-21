7972db66ac7e80381a91ecfa784bad1c2867d936
Ryan Knightly <reknightly@gmail.com>
2019-09-20 23:53:28 -0500

    Only make first loaded tab active

--- TenTabsExtension/src/tabber.js
+++ TenTabsExtension/src/tabber.js
@@ -1,6 +1,8 @@
 //Takes table of links and opens tabs for each of the links
 export function openLinks(links) {
+    var isFirst = true;
     for (url in links) {
-        chrome.tabs.create({ url: url });
+        chrome.tabs.create({ url: url, active: isFirst});
+        isFirst = false;
     }
 }
