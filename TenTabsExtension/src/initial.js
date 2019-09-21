9c5a3d3d52422aa92b8574d320c30941c8e0808b
V0rl3r <nicocrossi2000@gmail.com>
2019-09-21 18:00:16 -0500

    Slider now works

--- TenTabsExtension/src/initial.js
+++ TenTabsExtension/src/initial.js
@@ -3,7 +3,7 @@ document.addEventListener('DOMContentLoaded', function() {
    var searchForm = document.getElementsByName('searchform')[0];
    searchForm.addEventListener('submit', function() {
       chrome.storage.local.set({'searched': true});
-      chrome.storage.local.set({'newTab':null, 'links':null, 'numTabs':null})
+      chrome.storage.local.set({'newTab':null, 'links':null})
       chrome.tabs.getCurrent(function(tab) {
           chrome.tabs.remove(tab.id, function(){});
        });
@@ -14,8 +14,13 @@ var slider = document.getElementById("tabSlider");
 var tabCountDisplay = document.getElementById("tabCount")
 // TODO: save slider value
 slider.oninput = function() {
-   console.log(this.value);
    tabCountDisplay.innerHTML = this.value
-   // output.innerHTML = this.value;
+   chrome.storage.local.set({'numTabs':this.value}, function(){
+      console.log("Storage was updated with " + this.value)
+   })
+   chrome.storage.local.get('numTabs', function(result){
+      console.log(result.numTabs)
+   })
  }
 
+ chrome.storage.local.set({'numTabs':null})
