64a295b88dd578c60e52980cc563cc64473509f2
V0rl3r <nicocrossi2000@gmail.com>
2019-09-21 17:19:17 -0500

    Added coservation of new windows

--- TenTabsExtension/src/initial.js
+++ TenTabsExtension/src/initial.js
@@ -3,7 +3,7 @@ document.addEventListener('DOMContentLoaded', function() {
    var searchForm = document.getElementsByName('searchform')[0];
    searchForm.addEventListener('submit', function() {
       chrome.storage.local.set({'searched': true});
-      chrome.storage.local.set({'newTab':null, 'links':null})
+      chrome.storage.local.set({'newTab':null, 'links':null, 'numTabs':null})
       chrome.tabs.getCurrent(function(tab) {
           chrome.tabs.remove(tab.id, function(){});
        });
