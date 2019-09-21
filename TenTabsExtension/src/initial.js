0cdabb128862722f77ebafecc3bc8cf97a852600
Adam Zawierucha <zawiedev@gmail.com>
2019-09-21 07:34:35 -0500

    Made TenTabs tab close when form is submitted, instead of when submit button was pressed.

--- TenTabsExtension/src/initial.js
+++ TenTabsExtension/src/initial.js
@@ -1,7 +1,7 @@
 // Close the current tab when the search button is clicked
 document.addEventListener('DOMContentLoaded', function() {
-   var checkPageButton = document.getElementById('search_button');
-   checkPageButton.addEventListener('click', function() {
+   var searchForm = document.getElementById('searchForm');
+   searchForm.addEventListener('onsubmit', function() {
       chrome.tabs.getCurrent(function(tab) {
          chrome.tabs.remove(tab.id, function(){});
       });
