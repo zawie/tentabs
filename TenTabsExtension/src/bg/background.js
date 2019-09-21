ef7a4b007cde13a546798ab3abf858e91b95bbd2
V0rl3r <nicocrossi2000@gmail.com>
2019-09-21 11:12:44 -0500

    Added check to see if tab has finished loading

--- TenTabsExtension/src/bg/background.js
+++ TenTabsExtension/src/bg/background.js
@@ -19,12 +19,16 @@ chrome.browserAction.onClicked.addListener(function(activeTab)
 });
 
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-  console.log(changeInfo.url);
-}); 
-
+  console.log(changeInfo.status === 'complete');
+  if (changeInfo.status === 'complete'){
+    console.log("Tab " +tabId+ " has finished loading")
+  }
+});
+/*
 chrome.tabs.onActivated.addListener(function(activeInfo) {
  // how to fetch tab url using activeInfo.tabid
  chrome.tabs.get(activeInfo.tabId, function(tab){
     console.log(tab.url);
  });
 }); 
+*/
