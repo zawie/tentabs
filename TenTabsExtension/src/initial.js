cdd3dd6578bb7c5b4927fa04df37c9cf793dd79a
Adam Zawierucha <zawiedev@gmail.com>
2019-09-20 22:41:40 -0500

    listener

---
+++ TenTabsExtension/src/initial.js
@@ -0,0 +1,10 @@
+//Handles the home page prompt, and submits a search request to submitter.js
+document.addEventListener('DOMContentLoaded', function() {
+
+    var checkPageButton = document.getElementById('search_button');
+  
+    checkPageButton.addEventListener('click', function() {
+       console.log("Hello!!!!!!!!!!!!!")
+    }, false);
+    
+  }, false);
--- initial.js
+++
@@ -1,4 +0,0 @@
-//Handles the home page prompt, and submits a search request to submitter.js
-export function initialize() {
-
-}
