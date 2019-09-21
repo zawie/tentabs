7e442580aa14e334d6e1cb3205d8df160ec2e9f5
Adam Zawierucha <zawiedev@gmail.com>
2019-09-21 09:46:27 -0500

    Tied listener to form submission FIXED

--- TenTabsExtension/src/initial.js
+++ TenTabsExtension/src/initial.js
@@ -1,7 +1,7 @@
 // Close the current tab when the search button is clicked
 document.addEventListener('DOMContentLoaded', function() {
-   var checkPageButton = document.getElementById('search_button');
-   checkPageButton.addEventListener('click', function() {
+   var searchForm = document.getElementsByName('searchform')[0];
+   searchForm.addEventListener('submit', function() {
       chrome.tabs.getCurrent(function(tab) {
          chrome.tabs.remove(tab.id, function(){});
       });
