98d4d45b90d2edb2868621b2194296e9a670c731
V0rl3r <nicocrossi2000@gmail.com>
2019-09-21 19:14:35 -0500

    Added slider desc

--- TenTabsExtension/src/initial.js
+++ TenTabsExtension/src/initial.js
@@ -11,10 +11,10 @@ document.addEventListener('DOMContentLoaded', function() {
 }, false);
 
 var slider = document.getElementById("tabSlider");
-var tabCountDisplay = document.getElementById("tabCount")
+//var tabCountDisplay = document.getElementById("tabCount")
 // TODO: save slider value
 slider.oninput = function() {
-   tabCountDisplay.innerHTML = this.value
+   //tabCountDisplay.innerHTML = this.value
    chrome.storage.local.set({'numTabs':this.value}, function(){
       console.log("Storage was updated with " + this.value)
    })
